b550bb9dd09932979ddc8b28bedf6bbb1cfbdc66
June Mbuvi <112569835+JuneMbuvi@users.noreply.github.com>
2024-06-11 17:59:57 +0300

    Update App.js

--- 411/src/App.js
+++ 411/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [ posts, setPosts] = useState([]);
+  useEffect(() => {clear
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get('https://api.rss2json.com/v1/api.json', {
+          params: {
+            rss_url: 'https://medium.com/feed/towards-data-science'
+          }
+        });
+        setPosts(response.data.items);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>The 411</h1>
+        <nav>
+          <a href="#about">About</a>
+          <a href="#Contacts">Contacts</a>
+          <a href="#posts">Posts</a>
+        </nav>
       </header>
+      <main>
+      {posts.length > 0 ? (
+        posts.map(post => (
+          <div key={post.guid} className={"post"}>
+            <h2>{post.title}</h2>
+            <p dangerouslySetInnerHTML={{ __html: post.contentSnippet }}></p>
+            <a href={post.link} target="_blank" rel="noopener noreferrer">Read More</a>
+          </div>
+        ))
+      ) : (
+        <p>Loading posts...</p>
+      )}
+      </main>
     </div>
   );
 }
 
 export default App;
+
