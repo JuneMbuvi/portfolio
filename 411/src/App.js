589ec1c83ffacd0397cdb768c7db484eb7f235d9
June Mbuvi <112569835+JuneMbuvi@users.noreply.github.com>
2024-07-03 16:08:11 +0300

    Updating App.js

--- 411/src/App.js
+++ 411/src/App.js
@@ -9,12 +9,9 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1>The 411</h1>
           <nav>
             <Link to="/">Home</Link>
             <Link to="/stories">Stories</Link>
-            <a href="#about">About</a>
-            <a href="#contacts">Contacts</a>
           </nav>
         </header>
         <Routes>
